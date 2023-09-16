577344cfb4125acecf0934eb58cc53e6ad9b90d5
wzseba <wzseba@gmail.com>
2023-09-16 12:36:37 -0300

    add: se agrego interfaz

--- src/parcialGeometria/Figura.java
+++ src/parcialGeometria/Figura.java
@@ -1,6 +1,6 @@
 package parcialGeometria;
 
-public abstract class Figura {
+public abstract class Figura implements Comparable<Figura>{
 	
 	private double area;
 	
@@ -8,7 +8,26 @@ public abstract class Figura {
 		this.area = area;
 	}
 	
+	/* Final: No se puede sobrescribir un método final en ninguna clase derivada*/
 	public final double getArea() {
 		return this.area;
 	}
+	
+	public double getTotalAreas() {
+		double total = 0;
+		return total += getArea();
+	}
+		
+	/*Interfaz comparable, determinar si una figura es mayor o menor que otra segun su area*/
+	@Override
+	public int compareTo(Figura f) {
+		return Double.compare(this.getArea(), f.getArea());
+	}
+
+	/*Sobreescritura del metodo toString*/
+	@Override
+	public String toString() {
+		return " area: " + area;
+	}
+	
 }
