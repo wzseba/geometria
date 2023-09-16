6bee2d02b867a3e2ac2bedb6fdba6a329302f767
wzseba <wzseba@gmail.com>
2023-09-16 12:39:58 -0300

    add: se agrego sobreescritura toString con metodo replaceAll

--- src/parcialGeometria/Cuadrado.java
+++ src/parcialGeometria/Cuadrado.java
@@ -4,6 +4,11 @@ public class Cuadrado extends Rectangulo {
 
 	public Cuadrado(double lado) {
 		super(lado, lado);
-		
+	}
+	
+	/*Sobreescritura del metodo toString*/
+	@Override
+	public String toString() {
+		return "Cuadrado" + super.toString().replaceAll("Rectangulo", "");
 	}
 }
