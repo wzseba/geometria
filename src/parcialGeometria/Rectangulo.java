8fcfbb9293543a9c314a038a60225d76eab8c0bf
wzseba <wzseba@gmail.com>
2023-10-01 12:45:33 -0300

    add: se agrega constructor con la sobreescritura de metodos

--- src/parcialGeometria/Rectangulo.java
+++ src/parcialGeometria/Rectangulo.java
@@ -1,15 +1,28 @@
 package parcialGeometria;
 
 public class Rectangulo extends Figura {
+
+	private Punto puntoSupDerecho;
 	
-	public Rectangulo(double base, double altura) {
-		super(base * altura );
-		
+	public Rectangulo(double x1, double y1, double x2, double y2) {
+		super(x1,y1);
+		this.puntoSupDerecho = new Punto(x2,y2);
 	}
-	
-	/*Sobreescritura del metodo toString*/
+
 	@Override
-	public String toString() {
-		return "Rectangulo" + super.toString();
+	public void desplazar(double deltax, double deltay) {
+		super.getPunto().desplazar(deltax, deltay);
+		this.puntoSupDerecho.desplazar(deltax, deltay);
 	}
+
+	@Override
+	public double getArea() {
+		double base = this.puntoSupDerecho.getX() - super.getPunto().getX();
+		System.out.println(base);
+		double altura = this.puntoSupDerecho.getY() - super.getPunto().getY();
+		System.out.println(altura);
+		return base*altura; 
+	}
+	
+	
 }
