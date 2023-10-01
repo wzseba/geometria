0d470bf4103c6375bad2ecb783b0099c08f07028
wzseba <wzseba@gmail.com>
2023-10-01 12:44:16 -0300

    add: se agrega constructor

--- src/parcialGeometria/Cuadrado.java
+++ src/parcialGeometria/Cuadrado.java
@@ -2,13 +2,7 @@ package parcialGeometria;
 
 public class Cuadrado extends Rectangulo {
 
-	public Cuadrado(double lado) {
-		super(lado, lado);
-	}
-	
-	/*Sobreescritura del metodo toString*/
-	@Override
-	public String toString() {
-		return "Cuadrado" + super.toString().replaceAll("Rectangulo", "");
+	public Cuadrado(double x, double y, double lado) {
+		super(x, y, x+lado, y+lado);
 	}
 }
